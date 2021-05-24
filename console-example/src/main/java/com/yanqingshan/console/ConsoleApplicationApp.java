64e32de1871c367cf8ac8167b50e8e68e32ecd53
xiaose <qingshanking@vip.qq.com>
2021-05-24 12:32:37 +0800

    fix:控制台输出;时间测试

---
+++ console-example/src/main/java/com/yanqingshan/console/ConsoleApplicationApp.java
@@ -0,0 +1,11 @@
+package com.yanqingshan.console;
+
+/**
+ * 启动类 默认方法
+ *
+ * @author yanqs
+ * @date 2021-05-24
+ */
+public class ConsoleApplicationApp {
+
+}
---
+++ console-example/src/main/java/com/yanqingshan/console/date/DateTest.java
@@ -0,0 +1,47 @@
+package com.yanqingshan.console.date;
+
+
+import cn.hutool.core.lang.Console;
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.core.util.StrUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 时间测试
+ *
+ * @author yanqs
+ * @date 2021-05-24
+ */
+public class DateTest {
+    public void write() {
+        List<String> a = new ArrayList<String>();
+        a.add("2021-1");
+        Console.log(RandomUtil.randomNumbers(4));
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder help = new StringBuilder();
+        help.append("请输入");
+        System.out.println(help.toString());
+        Integer sj = 0;
+        Integer year = 2021;
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StrUtil.isNotBlank(ipt)) {
+                Console.log(ipt);
+                sj = Integer.valueOf(ipt);
+            }
+        }
+
+        for (Integer i = 0; i < 6; i++) {
+            Integer js = sj - i;
+            if (js <= 0) {
+                year = year - 1;
+                js = 12 + js;
+            }
+            Console.log(year + "-" + js);
+            //loginTime.add();
+        }
+    }
+}
