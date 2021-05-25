81ea47e1a5089fceb1e856128ae56e14c1714424
xiaose <qingshanking@vip.qq.com>
2021-05-26 00:06:16 +0800

    fix:websocket

---
+++ websocket-examples/src/main/java/com/yanqingshan/springboot/websocketexamples/WebsocketExamplesApplication.java
@@ -0,0 +1,19 @@
+package com.yanqingshan.springboot.websocketexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动类
+ *
+ * @author yanqs
+ * @date 2021-05-25
+ */
+@SpringBootApplication
+public class WebsocketExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketExamplesApplication.class, args);
+    }
+
+}
