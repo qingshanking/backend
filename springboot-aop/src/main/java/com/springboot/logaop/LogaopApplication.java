83f4fc8ccb8292f2191ac02966007049a708032a
xiaose <qingshanking@vip.qq.com>
2021-12-13 16:05:52 +0800

    feat:SpringBoot AOP日志切面

---
+++ springboot-aop/src/main/java/com/springboot/logaop/LogaopApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.logaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogaopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogaopApplication.class, args);
+    }
+
+}
